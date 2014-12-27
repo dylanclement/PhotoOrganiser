ca0207e971601f9d5cd919cec8b9d576ea155864
Dylan Clement <dylanclement@gmail.com>
2014-12-27 22:09:33 +1100

    Initial commit

---
+++ PhotoOrganiser/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace PhotoOrganiser
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sourceFolderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.destinationFolderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.sourceFolderText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sourceBrowseButton = new System.Windows.Forms.Button();
+            this.destinationBrowseButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.destinationFolderText = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.resultGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.resultGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sourceFolderText
+            // 
+            this.sourceFolderText.Location = new System.Drawing.Point(112, 22);
+            this.sourceFolderText.Name = "sourceFolderText";
+            this.sourceFolderText.Size = new System.Drawing.Size(441, 20);
+            this.sourceFolderText.TabIndex = 0;
+            this.sourceFolderText.Text = "D:\\Temp\\Dylan Pictures";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input Folder:";
+            // 
+            // sourceBrowseButton
+            // 
+            this.sourceBrowseButton.Location = new System.Drawing.Point(579, 20);
+            this.sourceBrowseButton.Name = "sourceBrowseButton";
+            this.sourceBrowseButton.Size = new System.Drawing.Size(45, 23);
+            this.sourceBrowseButton.TabIndex = 2;
+            this.sourceBrowseButton.Text = "...";
+            this.sourceBrowseButton.UseVisualStyleBackColor = true;
+            this.sourceBrowseButton.Click += new System.EventHandler(this.sourceBrowseButton_Click);
+            // 
+            // destinationBrowseButton
+            // 
+            this.destinationBrowseButton.Location = new System.Drawing.Point(579, 55);
+            this.destinationBrowseButton.Name = "destinationBrowseButton";
+            this.destinationBrowseButton.Size = new System.Drawing.Size(45, 23);
+            this.destinationBrowseButton.TabIndex = 5;
+            this.destinationBrowseButton.Text = "...";
+            this.destinationBrowseButton.UseVisualStyleBackColor = true;
+            this.destinationBrowseButton.Click += new System.EventHandler(this.destinationBrowseButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Destination Folder:";
+            // 
+            // destinationFolderText
+            // 
+            this.destinationFolderText.Location = new System.Drawing.Point(112, 57);
+            this.destinationFolderText.Name = "destinationFolderText";
+            this.destinationFolderText.Size = new System.Drawing.Size(441, 20);
+            this.destinationFolderText.TabIndex = 3;
+            this.destinationFolderText.Text = "D:\\Temp\\Test";
+            // 
+            // goButton
+            // 
+            this.goButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.goButton.Location = new System.Drawing.Point(549, 93);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 6;
+            this.goButton.Text = "&Ok";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // resultGridView
+            // 
+            this.resultGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.resultGridView.Location = new System.Drawing.Point(16, 142);
+            this.resultGridView.Name = "resultGridView";
+            this.resultGridView.Size = new System.Drawing.Size(619, 168);
+            this.resultGridView.TabIndex = 8;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 322);
+            this.Controls.Add(this.resultGridView);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.destinationBrowseButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.destinationFolderText);
+            this.Controls.Add(this.sourceBrowseButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sourceFolderText);
+            this.Name = "MainForm";
+            this.Text = "Photo Organiser";
+            ((System.ComponentModel.ISupportInitialize)(this.resultGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog sourceFolderBrowserDialog;
+        private System.Windows.Forms.FolderBrowserDialog destinationFolderBrowserDialog;
+        private System.Windows.Forms.TextBox sourceFolderText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button sourceBrowseButton;
+        private System.Windows.Forms.Button destinationBrowseButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox destinationFolderText;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.DataGridView resultGridView;
+    }
+}
+
---
+++ PhotoOrganiser/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoOrganiser
+{
+    public partial class MainForm : Form
+    {
+        public class ListEntry
+        {
+            [DisplayName("File Name")]
+            public string FileName { get; set; }
+            [DisplayName("Destination")]
+            public string Destination { get; set; }
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private IList<ListEntry> filesToCopy = new List<ListEntry>();
+        private static Regex r = new Regex(":");
+
+        //retrieves the datetime WITHOUT loading the whole image
+        public static DateTime GetDateTakenFromImage(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (Image myImage = Image.FromStream(fs, false, false))
+            {
+                if (myImage.PropertyIdList.Any(x => x == 36867))
+                {
+                    PropertyItem propItem = myImage.GetPropertyItem(36867);
+                    string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                    return DateTime.Parse(dateTaken);
+                } 
+                else
+                {
+                    MessageBox.Show("Unable to find date data for image " + path + " : " + GetDateTakenFromFile(path));
+                    return GetDateTakenFromFile(path);
+                }
+            }
+        }
+
+        public static DateTime GetDateTakenFromFile(string path)
+        {
+            return File.GetLastWriteTime(path);
+        }
+
+
+        private void sourceBrowseButton_Click(object sender, EventArgs e)
+        {
+            if (sourceFolderBrowserDialog.ShowDialog() == DialogResult.OK) 
+            {
+                sourceFolderText.Text = sourceFolderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void destinationBrowseButton_Click(object sender, EventArgs e)
+        {
+            if (destinationFolderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                destinationFolderText.Text = destinationFolderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            filesToCopy.Clear();
+            ProcessDirectory(sourceFolderText.Text, destinationFolderText.Text);
+            resultGridView.DataSource = filesToCopy.ToArray();
+            CopyFiles(filesToCopy);
+        }
+
+
+        private void CopyFiles(IList<ListEntry> items)
+        {
+            foreach (var item in items)
+            {
+                var fi = new FileInfo(item.FileName);
+                Directory.CreateDirectory(Path.GetDirectoryName(item.Destination));
+                try
+                {
+                    fi.CopyTo(item.Destination);
+                }
+                catch (System.IO.IOException)
+                {
+                    // File Already exists, ignore
+                }
+            }
+        }
+
+        // Process all files in the directory passed in, recurse on any directories  
+        // that are found, and process the files they contain. 
+        public void ProcessDirectory(string targetDirectory, string destinationDirectory)
+        {
+            if (!Directory.Exists(targetDirectory) || !Directory.Exists(destinationDirectory))
+            {
+                MessageBox.Show("Invalid directory");
+                return;
+            }
+            // Process the list of files found in the directory. 
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+                ProcessFile(fileName);
+
+            // Recurse into subdirectories of this directory. 
+            string[] subdirectoryEntries = Directory.GetDirectories(targetDirectory);
+            foreach (string subdirectory in subdirectoryEntries)
+                ProcessDirectory(subdirectory, destinationDirectory);
+        }
+
+        private string GetDestinationName(string path, string file, DateTime date)
+        {
+            return string.Format("{0}\\{1}-{2}-{3}\\{4}", path, date.Year, date.Month.ToString().PadLeft(2, '0'), date.Day.ToString().PadLeft(2, '0' ), Path.GetFileName(file));
+        }
+
+        // Insert logic for processing found files here. 
+        public void ProcessFile(string path)
+        {
+            var extension = Path.GetExtension(path).ToLower();
+            if (extension == ".jpg")
+            {
+                filesToCopy.Add(new ListEntry { FileName = path, Destination = GetDestinationName(destinationFolderText.Text, path, GetDateTakenFromImage(path)) });
+            }
+            else if (extension == ".mpg" || extension == ".mp4" || extension == ".mov")
+            {
+                filesToCopy.Add(new ListEntry { FileName = path, Destination = GetDestinationName(destinationFolderText.Text, path, GetDateTakenFromFile(path)) });
+            }
+        }
+    }
+}
