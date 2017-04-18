fef885a08cc3e23b94c8815dd1e517a92cf33663
Dylan <dylanclement@gmail.com>
2017-04-18 22:07:33 +1000

    initial commit

---
+++ Photo-organiser.js/README.md
@@ -0,0 +1,3 @@
+﻿# Photo-organiser.js
+
+
---
+++ Photo-organiser.js/app.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+var path = require('path');
+var ExifImage = require('exif').ExifImage;
+var moment = require('moment');
+var inputDir = 'E:\\Pictures\\2016\\2016-09-11';
+var path = require('path');
+try {
+    // Loop through all the files in the temp directory
+    fs.readdir(inputDir, function (err, files) {
+        if (err) {
+            console.error("Could not list the directory.", err);
+            process.exit(1);
+        }
+        files.forEach(function (file, index) {
+            var ext = path.extname(file);
+            if ([".jpg", ".jpeg"].indexOf(path.extname(file)) != -1) {
+                new ExifImage({ image: inputDir + "\\" + file }, function (error, exifData) {
+                    if (error)
+                        console.log('Error: ' + error.message);
+                    else {
+                        var date = exifData.exif.CreateDate;
+                        var gpsdate = exifData.gps.GPSDateStamp;
+                        console.log(file, ' date = ', moment(date, "YYYY:MM:DD hh:mm:ss"), moment(gpsdate, "YYYY:MM:DD")); // Do something with your data!
+                    }
+                });
+            }
+        });
+    });
+}
+catch (error) {
+    console.log('Error: ' + error.message);
+}
+//# sourceMappingURL=app.js.map
